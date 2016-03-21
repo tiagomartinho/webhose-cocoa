aa83b56c25ab76fc054aa3379d9dc7f5d4aba3f3
tiagomartinho <tiago.martinho@xpeppers.com>
2016-03-21 21:30:47 +0100

    adds HTTPStubs to test server error request

---
+++ webhose-cocoa/Pods/Target Support Files/Pods-WebhoseTests-OHHTTPStubs/Pods-WebhoseTests-OHHTTPStubs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WebhoseTests_OHHTTPStubs : NSObject
+@end
+@implementation PodsDummy_Pods_WebhoseTests_OHHTTPStubs
+@end
--- webhose-cocoa/WebhoseTests/WebhoseTests.swift
+++ webhose-cocoa/WebhoseTests/WebhoseTests.swift
@@ -1,14 +1,21 @@
-import XCTest
 @testable import Webhose
+import XCTest
+import OHHTTPStubs
 
 class WebhoseTests: XCTestCase {
     let aKey = "aKey"
     let aQuery = "aQuery"
-    let expectedNumberOfResults = 123
-
-    func testSimpleRequest() {
+    
+    func testError() {
+        stubsError()
         let client = WebhoseClient(key: aKey)
         let response = client.search(aQuery)
-        XCTAssertEqual(expectedNumberOfResults, response.posts.count)
+        XCTAssertEqual(0, response.posts.count)
+    }
+    
+    func stubsError() {
+        stub(isHost("webhose.io")) { _ in
+            return fixture("", status: 500, headers: ["Content-Type":"application/json"])
+        }
     }
 }
